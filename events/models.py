14da0cd3f21f00211fa032c102b1ef084078334a
Sediqa <sadat.hadid2000@gmail.com>
2023-07-08 17:41:22 +0000

    Create events model

---
+++ events/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
+
+class Event(models.Model):
+    """
+    Event model, related to User
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    location = models.TextField(blank=True)
+    event_date = models.DateField(blank=False)
+    starts_at = models.DateTimeField(auto_now_add=True)
+    ends_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default-event_lu9ey8', blank=True
+    )
+
+
+    class Meta:
+    ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
