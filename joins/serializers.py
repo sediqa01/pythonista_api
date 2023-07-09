dbb4dbb12329e85943c85f33c9660880cb505d7a
Sediqa <sadat.hadid2000@gmail.com>
2023-07-09 16:56:46 +0000

    Add validation to stop a user to Join the same event twice

--- joins/serializers.py
+++ joins/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Join
 
@@ -13,3 +14,15 @@ class JoinSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'event', 'created_at'
         ]
+
+
+        def create(self, validated_data):
+        """
+        Validation to stop a user to Join the same event twice
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You are already Joined!'
+        })
