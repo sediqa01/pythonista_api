94cb85fd38eb7cf7d2cdc220fdf9a66ae9974802
Sediqa <sadat.hadid2000@gmail.com>
2023-07-10 13:38:55 +0000

    Add extra fields to Profiles by annotating the post views queryset

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
+from comments.models import Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """
+    Post serializer
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -27,9 +35,20 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
 
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'content', 'created_at', 'updated_at',
-            'image', 'is_owner', 'profile_id', 'profile_image',]
+            'image', 'is_owner', 'profile_id', 'profile_image',
+            'like_id', 'likes_count', 'comments_count',
+        ]
