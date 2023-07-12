2a1add0cbeeddf9f1ad5dc6550b1d5dfc3036a5d
Sediqa <sadat.hadid2000@gmail.com>
2023-07-12 16:29:28 +0000

    Add test for followers detail view

--- followers/tests.py
+++ followers/tests.py
@@ -6,7 +6,7 @@ from rest_framework.test import APITestCase
 
 class FollowerListViewTests(APITestCase):
     """
-    Tests for the Follower model list view
+    Follower list view tests
     """
     def setUp(self):
         pythonista = User.objects.create_user(
@@ -39,3 +39,35 @@ class FollowerListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         count = Follower.objects.count()
         self.assertEqual(count, 2)
+
+
+class FollowerDetailViewTests(APITestCase):
+    """
+    Follower detail view tests
+    """
+    def setUp(self):
+        pythonista = User.objects.create_user(
+            username='pythonista', password='pp5.react')
+        developer = User.objects.create_user(
+            username='developer', password='django.rf')
+        programmer = User.objects.create_user(
+            username='programmer', password='react.js')
+        Follower.objects.create(owner=pythonista, followed=developer)
+        Follower.objects.create(owner=pythonista, followed=programmer)
+
+    def test_can_retrieve_follow_using_valid_id(self):
+        response = self.client.get('/followers/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_not_retrieve_follow_using_invalid_id(self):
+        response = self.client.get('/followers/55/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_user_unfollow_a_user(self):
+        self.client.login(username='pythonista', password='pp5.react')
+        response = self.client.delete('/followers/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_logged_out_user_can_not_unfollow(self):
+        response = self.client.delete('/followers/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
